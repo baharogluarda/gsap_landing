10689e60642447dc3351aae0604d2176aeedf814
Arda Baharoğlu <arda.900bhrgl@gmail.com>
2026-01-16 00:42:26 +0100

    Navbar Added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import gsap from "gsap";
+import {ScrollTrigger, SplitText} from "gsap/all";
+import Navbar from "./components/Navbar.jsx";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+    return (<main>
+        <Navbar/>
+    </main>)
+}
+export default App
